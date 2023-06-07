ec43f745b6a8dfd13a850a4f6049310ae3ca9cf1
wsh096 <wsh096@gmail.com>
2023-06-07 17:29:49 +0900

    feat ✨ 예약 기능 엔터티 제작.
    1. room 경우, Accommodation의 등록을 고려해 미리 생성
    
    2. 이를 고려해 user 요소도 추가적으로 수정 예정.

--- src/main/java/com/zerobase/yogizogi/book/domain/entity/book.java
+++ src/main/java/com/zerobase/yogizogi/book/domain/entity/book.java
@@ -1,8 +1,31 @@
 package com.zerobase.yogizogi.book.domain.entity;
 
+import com.zerobase.yogizogi.user.domain.entity.AppUser;
+import java.time.LocalDateTime;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private AppUser user;
 
+//    @ManyToOne
+//    private Rooom room;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime endDate;
+    private boolean accept;
+    private boolean cancel;
+    private int people;
+    private int payAmount;
 }
