dba3b1893fc4d51029f295051fb78dc9d83d2764
wsh096 <wsh096@gmail.com>
2023-06-12 21:28:30 +0900

    feat : ✨ bookDto_test_code 작성
    1. bookDto from 불필요 변수(id_pk) 삭제.
    
    2. 불필요 메서드 reviewRegistered(boolean) 삭제.
    
    3. 빌더 추가
    
    4. 예약Dto 값이 제대로 생성되는지 테스트(통과)
    
    5. /then<- format 관련 논의 필요.
       assertEquals로 할지, assertAll을 할지 여부
    
    footer fix : 🚑  BookTest depth 수정.

--- src/main/java/com/zerobase/yogizogi/book/dto/BookDto.java
+++ src/main/java/com/zerobase/yogizogi/book/dto/BookDto.java
@@ -1,8 +1,8 @@
 package com.zerobase.yogizogi.book.dto;
 
-import com.zerobase.yogizogi.book.domain.entity.Book;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,24 +12,13 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Builder
 public class BookDto {
-    private Long id;//bookId
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     private int people;
     private int payAmount;
-    private boolean reviewRegistered;
-    public static BookDto fromBook(Book book) {
-        BookDto bookDto = new BookDto();
-        bookDto.setId(book.getId());
-        bookDto.setStartDate(book.getStartDate());
-        bookDto.setEndDate(book.getEndDate());
-        bookDto.setPeople(book.getPeople());
-        bookDto.setPayAmount(book.getPayAmount());
-        bookDto.setReviewRegistered(book.isReviewRegistered());
-        return bookDto;
-    }
+
 }
--- src/test/java/com/zerobase/yogizogi/book/domain/entity/BookTest.java
+++ src/test/java/com/zerobase/yogizogi/book/domain/entity/BookTest.java
@@ -8,23 +8,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class BookTest {
-@Test
-@DisplayName("예약 엔터티의 값이 제대로 생성되는지 테스트")
-void createBook(){
-    //given
-    Book book = Book.builder().bookName("홍길동").reviewRegistered(false).userId(3L).roomId(2L)
-        .startDate(LocalDate.now()).endDate(LocalDate.now().plusDays(1))
-        .payAmount(1000).people(4).accommodationId(1L).build();
-    //when
-    // then
-    assertEquals("홍길동", book.getBookName());
-    assertFalse(book.isReviewRegistered());
-    assertEquals(3L, book.getUserId());
-    assertEquals(2L, book.getRoomId());
-    assertEquals(LocalDate.now(),book.getStartDate());
-    assertEquals(LocalDate.now().plusDays(1),book.getEndDate());
-    assertEquals(1000, book.getPayAmount());
-    assertEquals(4, book.getPeople());
-    assertEquals(1L, book.getAccommodationId());
+    @Test
+    @DisplayName("예약 엔터티의 모든 값이 제대로 생성되는지 테스트")
+    void createBook(){
+        //given
+        Book book = Book.builder().bookName("홍길동").reviewRegistered(false).userId(3L).roomId(2L)
+            .startDate(LocalDate.now()).endDate(LocalDate.now().plusDays(1))
+            .payAmount(1000).people(4).accommodationId(1L).build();
+        //when
+        // then
+        assertEquals("홍길동", book.getBookName());
+        assertFalse(book.isReviewRegistered());
+        assertEquals(3L, book.getUserId());
+        assertEquals(2L, book.getRoomId());
+        assertEquals(LocalDate.now(),book.getStartDate());
+        assertEquals(LocalDate.now().plusDays(1),book.getEndDate());
+        assertEquals(1000, book.getPayAmount());
+        assertEquals(4, book.getPeople());
+        assertEquals(1L, book.getAccommodationId());
     }
 }
