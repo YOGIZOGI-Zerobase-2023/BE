7f10bf378b8e329eda82542b1afa61fe085edce0
wsh096 <wsh096@gmail.com>
2023-06-14 11:08:26 +0900

    fix : 🚑 build.gradle 오타 삭제
    1. sas 라는게 들어가 있었는데 삭제
    
    2. ErrorCode 마지막 포맷 조금 수정.

--- src/main/java/com/zerobase/yogizogi/global/exception/ErrorCode.java
+++ src/main/java/com/zerobase/yogizogi/global/exception/ErrorCode.java
@@ -40,7 +40,7 @@ public enum ErrorCode {
     USER_NOT_ALLOW_ACCESS(HttpStatus.BAD_REQUEST, "유저는 방 등록을 할 수 없습니다."),
     NOT_FOUND_ACCOMMODATION(HttpStatus.BAD_REQUEST, "존재하지 않는 숙소입니다."),
 
-    endSample(HttpStatus.BAD_REQUEST, "존재하지 않는 유저입니다.");
+    endSample(HttpStatus.BAD_REQUEST, "마무리 샘플입니다.");
 
 
 
