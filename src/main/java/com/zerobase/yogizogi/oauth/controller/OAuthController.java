4aa6ce6aadfca78e24a6e420efe61a0dda82ea9f
wsh096 <wsh096@gmail.com>
2023-06-15 00:05:43 +0900

    init: 🎉 userLogOut 기능 관련 기본 내용 커밋.
    1. Redis 등 설정해 줘야 할 부분이 많아 남겨둠.
    
    2. 포맷 먼저 잡고, JWT 등의 요소들이 바뀌어야 할 것으로 보임.
    현재 토큰 1개, 추후 토큰 2개

--- src/main/java/com/zerobase/yogizogi/oauth/controller/OAuthController.java
+++ src/main/java/com/zerobase/yogizogi/oauth/controller/OAuthController.java
@@ -5,7 +5,7 @@ import com.zerobase.yogizogi.oauth.service.OauthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class OAuthController {
 
     private final OauthService oauthService;
 
-    @GetMapping("/kakao-login")
+    @PostMapping("/kakao-login")
     public ResponseEntity<?> oAuthCallback(String code) {
         return ResponseEntity.ok(oauthService.oAuthCallBack(code));
     }
