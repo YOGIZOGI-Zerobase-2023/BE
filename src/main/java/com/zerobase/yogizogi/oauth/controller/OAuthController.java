9400c2d8ba97a8cea709416d6b72f721c5186cdb
wsh096 <wsh096@gmail.com>
2023-06-16 11:43:15 +0900

    fix : 🚑 OAuthController 수정

--- src/main/java/com/zerobase/yogizogi/oauth/controller/OAuthController.java
+++ src/main/java/com/zerobase/yogizogi/oauth/controller/OAuthController.java
@@ -1,10 +1,10 @@
 package com.zerobase.yogizogi.oauth.controller;
 
 
+import com.zerobase.yogizogi.global.ApiResponse;
 import com.zerobase.yogizogi.oauth.service.OauthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +19,7 @@ public class OAuthController {
     private final OauthService oauthService;
 
     @GetMapping("/kakao-login")
-    public @ResponseBody ResponseEntity<?> oAuthCallback(String code) {
-        return ResponseEntity.ok(oauthService.oAuthCallBack(code));
+    public @ResponseBody ApiResponse<?> oAuthCallback(String code) {
+        return oauthService.oAuthCallBack(code);
     }
 }
