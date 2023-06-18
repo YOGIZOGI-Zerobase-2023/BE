a2d003d1b00998e041cd0ed088d78ebb6080d5ef
wsh096 <wsh096@gmail.com>
2023-06-18 20:26:20 +0900

    test commit

---
+++ src/main/java/com/zerobase/yogizogi/user/controller/testController.java
@@ -0,0 +1,13 @@
+package com.zerobase.yogizogi.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class testController {
+@GetMapping("/test")
+public String test(){
+    return "login";
+}
+}
