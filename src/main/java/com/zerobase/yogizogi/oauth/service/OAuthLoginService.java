b14d8ff3750265a698f6d49527a069c059c33758
wsh096 <wsh096@gmail.com>
2023-06-20 18:13:13 +0900

    fix : 🚑 리다이렉트 검증 완료.
    토큰이 정상적으로 넘어가게 값을 입력 완료.
    
    footer : 주소의 경우, 임시적으로 넣어둔 거라 추후 변경 가능

--- src/main/java/com/zerobase/yogizogi/oauth/service/OAuthLoginService.java
+++ src/main/java/com/zerobase/yogizogi/oauth/service/OAuthLoginService.java
@@ -4,8 +4,6 @@ import com.zerobase.yogizogi.global.ApiResponse;
 import com.zerobase.yogizogi.global.ResponseCode;
 import com.zerobase.yogizogi.user.domain.entity.AppUser;
 import com.zerobase.yogizogi.user.token.JwtAuthenticationProvider;
-import java.util.Map;
-import java.util.TreeMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -18,18 +16,14 @@ public class OAuthLoginService {
 
     public ApiResponse<?> oAuthLogin(AppUser user) {
 
-        String[] emails = user.getEmail().split("_");
 
-        Map<String, Object> data = new TreeMap<>();
-        data.put("X-AUTH-TOKEN", provider.createToken(user.getEmail(), user.getId()));
-        data.put("email", emails[0]);
-        data.put("nickname", user.getNickName());
+        String token = provider.createToken(user.getEmail(), user.getId());
 
         return new ApiResponse<>(
             ResponseCode.RESPONSE_SUCCESS.getCode(),
             HttpStatus.OK,
             "SUCCESS",
-            data
+            token
         );
     }
 }
