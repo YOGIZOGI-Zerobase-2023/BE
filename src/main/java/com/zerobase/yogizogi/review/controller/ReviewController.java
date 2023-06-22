56f67961a0d86d00aaca193a548040650dbb7530
wsh096 <wsh096@gmail.com>
2023-06-23 01:10:07 +0900

    fix : bookService list 반환
    1. [프론트 요청] page 제거 완료.
    
    2. [프론트 요청] 결과값 반환을 위한 list 생성. //응답값 형태 변경은 아직 변경 안 함.
    
    3. [review] 관련 > 리뷰 작성 시 평점 변경 현재 값에 평점 새로 변환해서 하는 형태로 로직 형성.
    
    4. 모든 변수명 null 값 허용하는 object로 느슨하게 조절.
    
    5. config 폴더도 기본적인 global 설정이라는 생각 하에 config 폴더 위치 변경.
    
    6. 현재 예약 결과값을 반환하는 형태는 forEach 형태 즉시 stream으로 바꿀 예정.

--- src/main/java/com/zerobase/yogizogi/review/controller/ReviewController.java
+++ src/main/java/com/zerobase/yogizogi/review/controller/ReviewController.java
@@ -19,32 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/reviews")
+@RequestMapping("/api/accommodation/{accommodationId}/review")
 public class ReviewController {
 
     private final String TOKEN = "X-AUTH-TOKEN";
     private final ReviewService reviewService;
 
-    @GetMapping()
-    public ResponseEntity<?> reviewsList(@RequestParam(name = "id", defaultValue = "1") Long accommodationId,
+    @GetMapping("")
+    public ResponseEntity<?> reviewsList(
+        @PathVariable(name = "accommodationId") Long accommodationId,
         @RequestParam(name = "page", defaultValue = "0") int page,
-        @RequestParam(name = "size", defaultValue = "2") int size,// 예외 처리가 추가로 필요할 것으로 보임. id만 들어가면 에러.
-        @RequestParam(name = "sort", defaultValue = "id,desc") String sort){
+        @RequestParam(name = "pagesize", defaultValue = "2") int size,
+        @RequestParam(name = "sort", defaultValue = "id,desc") String sort) {
         String[] sortProperties = sort.split(",");
         Sort.Direction direction = sortProperties[1].equalsIgnoreCase("desc")
             ? Sort.Direction.DESC : Sort.Direction.ASC;
         Pageable pageable = PageRequest.of(page, size, Sort.by(direction, sortProperties[0]));
-        return ResponseEntity.ok(reviewService.reviewList(accommodationId,pageable));
+        return ResponseEntity.ok(reviewService.reviewList(accommodationId, pageable));
     }
 
-    @PostMapping()
-    public ResponseEntity<?> makeReview(@RequestHeader(name = TOKEN) String token,
+    @PostMapping("")
+    public ResponseEntity<?> makeReview(
+        @PathVariable(name = "accommodationId") Long accommodationId,
+        @RequestHeader(name = TOKEN) String token,
         @RequestBody ReviewForm reviewForm) {
-        return ResponseEntity.ok(reviewService.makeReview(token,reviewForm));
+        return ResponseEntity.ok(reviewService.makeReview(accommodationId, token, reviewForm));
     }
 
     @DeleteMapping("/{reviewId}")
-    public ResponseEntity<?> deleteReview(@RequestHeader(name = TOKEN) String token,
+    public ResponseEntity<?> deleteReview(
+        @PathVariable(name = "accommodationId") Long accommodationId,
+        @RequestHeader(name = TOKEN) String token,
         @PathVariable(name = "reviewId") Long reviewId) {
         return ResponseEntity.ok().body(reviewService.deleteReview(token, reviewId));
     }
