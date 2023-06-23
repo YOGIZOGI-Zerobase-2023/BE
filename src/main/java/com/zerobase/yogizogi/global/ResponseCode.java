6c592d37f0a8d575e47bd2514c187a6969dfe2e4
wsh096 <wsh096@gmail.com>
2023-06-23 15:35:36 +0900

    fix : ResponseCode 사용하는 걸로만 정리

--- src/main/java/com/zerobase/yogizogi/global/ResponseCode.java
+++ src/main/java/com/zerobase/yogizogi/global/ResponseCode.java
@@ -8,8 +8,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ResponseCode implements Code{
 
-    RESPONSE_DELETED(HttpStatus.NO_CONTENT, "DELETED"),
-    RESPONSE_CREATED(HttpStatus.CREATED, "CREATED"),
     RESPONSE_SUCCESS(HttpStatus.OK, "SUCCESS"),
     RESPONSE_FAIL(HttpStatus.BAD_REQUEST, "FAIL");
     private final HttpStatus status;
