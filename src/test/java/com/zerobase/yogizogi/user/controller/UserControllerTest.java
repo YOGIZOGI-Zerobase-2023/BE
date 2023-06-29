3191110d113e54cb24cfbf2fc3bbe8997816746d
wsh096 <wsh096@gmail.com>
2023-06-30 00:28:51 +0900

    feat : ✨ userLogin Controller 테스트 코드 작성
    1. LogInForm class 생성자 추가는 테스트를 위함
    
    footer 이메일 인증의 경우, 메인페이지 리다이렉트 URL을 넘기는 방법을 좀 더 찾아 봐야 할 것으로 보임

--- src/test/java/com/zerobase/yogizogi/user/controller/UserControllerTest.java
+++ src/test/java/com/zerobase/yogizogi/user/controller/UserControllerTest.java
@@ -1,23 +1,27 @@
 package com.zerobase.yogizogi.user.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.zerobase.yogizogi.global.ApiResponse;
 import com.zerobase.yogizogi.global.ResponseCode;
+import com.zerobase.yogizogi.user.domain.entity.AppUser;
+import com.zerobase.yogizogi.user.domain.model.LogInForm;
 import com.zerobase.yogizogi.user.domain.model.UserSignUpForm;
 import com.zerobase.yogizogi.user.repository.UserRepository;
 import com.zerobase.yogizogi.user.service.UserService;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserControllerTest {
@@ -27,7 +31,12 @@ class UserControllerTest {
 
     @Mock
     private UserRepository userRepository;
-
+    private UserController userController;
+    private String mainPageUrl;
+    @BeforeEach
+    void setUp() {
+        userController = new UserController(userService, userRepository);
+    }
     @Test
     @DisplayName("회원가입_성공")
     void signUp_Success() {
@@ -35,10 +44,7 @@ class UserControllerTest {
         UserController userController = new UserController(userService, userRepository);
         UserSignUpForm userSignUpForm = new UserSignUpForm("test@example.com", "testUser", "password");
 
-        // Mock userRepository.findByEmail to return an empty Optional, indicating that the email is not registered yet.
         when(userRepository.findByEmail(userSignUpForm.getEmail())).thenReturn(Optional.empty());
-
-        // Mock userRepository.findByNickName to return an empty Optional, indicating that the nickname is not registered yet.
         when(userRepository.findByNickName(userSignUpForm.getNickName())).thenReturn(Optional.empty());
 
         // when
@@ -53,5 +59,38 @@ class UserControllerTest {
         assertEquals(expectedData, response.getBody().getData());
 
         verify(userService, times(1)).signUp(userSignUpForm);
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    void login_Success() {
+        // given
+        LogInForm logInForm = new LogInForm("test@example.com", "password");
+
+        String token = "token123";
+        String email = logInForm.getEmail();
+        String nickname = "testUser";
+
+        AppUser mockUser = new AppUser();
+        mockUser.setNickName(nickname);
+
+        when(userService.login(logInForm)).thenReturn(token);
+        when(userRepository.findByEmail(logInForm.getEmail())).thenReturn(Optional.of(mockUser));
+
+        // when
+        ResponseEntity<ApiResponse<Object>> response = userController.login(logInForm);
+
+        // then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseCode.RESPONSE_SUCCESS.toString(), response.getBody().getCode());
+
+        Map<String, String> expectedData = new HashMap<>();
+        expectedData.put("X-AUTH-TOKEN", token);
+        expectedData.put("email", email);
+        expectedData.put("nickname", nickname);
+        assertEquals(expectedData, response.getBody().getData());
+
+        verify(userService, times(1)).login(logInForm);
+        verify(userRepository, times(1)).findByEmail(logInForm.getEmail());
     }
 }
