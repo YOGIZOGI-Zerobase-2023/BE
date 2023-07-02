8fa3bc741bd1145641f35f5efd5904c71c8695dd
wsh096 <wsh096@gmail.com>
2023-07-03 00:57:06 +0900

    fix : 🚑 BookControllerTest
    1. 리스트
    
    2. 예약하기/삭제하기

--- src/test/java/com/zerobase/yogizogi/book/controller/BookControllerTest.java
+++ src/test/java/com/zerobase/yogizogi/book/controller/BookControllerTest.java
@@ -1,80 +1,76 @@
 package com.zerobase.yogizogi.book.controller;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zerobase.yogizogi.book.domain.model.BookForm;
 import com.zerobase.yogizogi.book.service.BookService;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.Collections;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-class BookControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
 
-    @InjectMocks
-    private BookController bookController;
-    @Mock
-    private BookService bookService;
+    @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(bookController).build();
-    }
+    @MockBean
+    private BookService bookService;
 
     @Test
-    @DisplayName("GET /books 예약 page 반환")
-    void testMyBook() throws Exception {
+    @DisplayName("예약리스트 반환")
+    public void testMyBook() throws Exception {
         // given
+        long userId = 1L;
         String token = "testToken";
-        int page = 0;
-        int size = 2;
-        String sort = "id,desc";
-        // when/then
-        mockMvc.perform(get("/books")
-                .header("X-AUTH-TOKEN", token)
-                .param("page", String.valueOf(page))
-                .param("size", String.valueOf(size))
-                .param("sort", sort)
-                .contentType(MediaType.APPLICATION_JSON))
+        given(this.bookService.myBookList(userId, token)).willReturn(Collections.emptyList());
+
+        // when
+        this.mockMvc.perform(get("/api/user/" + userId + "/mybook")
+                .header("X-AUTH-TOKEN", token))
+            // then
             .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("POST /books 예약 만들기")
-    void testMakeBook() throws Exception {
+    @DisplayName("예약하기")
+    public void testMakeBook() throws Exception {
         // given
+        long accommodationId = 1L;
         String token = "testToken";
         BookForm bookForm = new BookForm();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String bookFormJson = objectMapper.writeValueAsString(bookForm);
-        // when/then
-        mockMvc.perform(post("/books")
+        // when
+        this.mockMvc.perform(post("/api/accommodation/" + accommodationId + "/book")
                 .header("X-AUTH-TOKEN", token)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(bookFormJson))
+                .content(new ObjectMapper().writeValueAsString(bookForm)))
+            // then
             .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("DELETE /books/{bookId} 특정 예약 삭제")
-    void testDeleteBook() throws Exception {
+    @DisplayName("예약 삭제")
+    public void testDeleteBook() throws Exception {
         // given
-        String token = "testToken";
+        long userId = 1L;
         long bookId = 1L;
-        // when/then
-        mockMvc.perform(delete("/books/{bookId}", bookId)
-                .header("X-AUTH-TOKEN", token)
-                .contentType(MediaType.APPLICATION_JSON))
+        String token = "testToken";
+        // when
+        this.mockMvc.perform(delete("/api/user/" + userId + "/mybook/" + bookId)
+                .header("X-AUTH-TOKEN", token))
+            // then
             .andExpect(status().isOk());
     }
 }
