77ac72007f852ad9522285046727b030dac2009e
wsh096 <wsh096@gmail.com>
2023-07-03 03:06:25 +0900

    fix : 🚑 BookServiceTest 제작중

--- src/main/java/com/zerobase/yogizogi/user/dto/UserDto.java
+++ src/main/java/com/zerobase/yogizogi/user/dto/UserDto.java
@@ -2,6 +2,8 @@ package com.zerobase.yogizogi.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @Getter
